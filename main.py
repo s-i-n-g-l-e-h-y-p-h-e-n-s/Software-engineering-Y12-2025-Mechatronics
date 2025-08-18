249b981cc2f4607fd6b9b1645fccaa493d7b9e6c
TariniT <tarini.thirumala@education.nsw.gov.au>
2025-08-19 09:11:49 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+ if shift:
+            entry_mode |= self.LCD_ENTRY_SHIFT
+        self.hal_write_command(entry_mode)
+
+    def backlight_off(self):
+        self.hal_backlight_off()
+
+    def backlight_on(self):
+        self.hal_backlight_on()
+
+    def home(self):
+        self.hal_write_command(self.LCD_HOME)
+        self.hal_sleep(3)
+
+    def set_cursor(self, col, line):
+        addr = col & 0x3F
+        if line & 1:
+            addr += 0x40
+        if line & 2:
+            addr += 0x14
+        self.hal_write_command(self.LCD_DDRAM | addr)
+
+    def print(self, string):
+        for char in string:
+            self.hal_write_data(ord(char))
+
+    def custom_char(self, location, charmap):
+        self.hal_write_command(self.LCD_CGRAM | ((location & 7) << 3))
+        self.hal_sleep(5)
+        for i in range(8):
+            self.hal_write_data(charmap[i])
+        self.home()
+
+    def print_custom_char(self, location):
+        self.hal_write_data(location)
+
+    def hal_sleep(self, time_ms):
+        sleep_ms(time_ms)
