967065e281281f9ced90be3678236cf7b4f8c927
TariniT <tarini.thirumala@education.nsw.gov.au>
2025-08-19 09:18:41 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
- if shift:
-            entry_mode |= self.LCD_ENTRY_SHIFT
-        self.hal_write_command(entry_mode)
 
-    def backlight_off(self):
-        self.hal_backlight_off()
+from machine import I2C, Pin
+from LCD_LIBRARY import LCD_I2C
+import time
+import dht
 
-    def backlight_on(self):
-        self.hal_backlight_on()
+# The Raspberry Pi Pico pin (GP15) connected to the DHT11 sensor
+DHT11_PIN = 15
 
-    def home(self):
-        self.hal_write_command(self.LCD_HOME)
-        self.hal_sleep(3)
+# The I2C address of your LCD (Update if different)
+I2C_ADDR = 0x27  # Use the address found using the I2C scanner
 
-    def set_cursor(self, col, line):
-        addr = col & 0x3F
-        if line & 1:
-            addr += 0x40
-        if line & 2:
-            addr += 0x14
-        self.hal_write_command(self.LCD_DDRAM | addr)
+# Define the number of rows and columns on your LCD
+LCD_ROWS = 2
+LCD_COLS = 16
 
-    def print(self, string):
-        for char in string:
-            self.hal_write_data(ord(char))
+# Initialize the DHT11 sensor
+DHT11 = dht.DHT11(machine.Pin(DHT11_PIN))
 
-    def custom_char(self, location, charmap):
-        self.hal_write_command(self.LCD_CGRAM | ((location & 7) << 3))
-        self.hal_sleep(5)
-        for i in range(8):
-            self.hal_write_data(haha[i])
-        self.home()
+# Initialize I2C
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 
-    def print_custom_char(self, location):
-        self.hal_write_data(location)
+# Initialize LCD
+lcd = LCD_I2C(i2c, I2C_ADDR, LCD_ROWS, LCD_COLS)
 
-    def hal_sleep(self, time_ms):
-        sleep_ms(time_ms)
+# Setup function
+lcd.backlight_on()
+lcd.clear()
+
+# Custom character for the degree symbol
+degree_char = chr(0xDF)
+
+# Main loop: Read data from the DHT11 sensor and display on LCD every 2 seconds
+while True:
+    try:
+        DHT11.measure()
+        temperature = DHT11.temperature()  # Gets the temperature in Celsius
+        humidity = DHT11.humidity()  # Gets the relative humidity in %
+        print("Temperature: {:.2f}°C, Humidity: {:.2f}%".format(temperature, humidity))
+        lcd.clear()
+        lcd.set_cursor(0, 0) # Move to the beginning of the first row
+        lcd.print("Temp: {:.2f}{}C".format(temperature, degree_char))
+        lcd.set_cursor(0, 1)  # Move to the beginning of the second row
+        lcd.print("Humi: {:.2f}%".format(humidity))
+    except OSError as e:
+        print("Failed to read from DHT11 sensor:", e)
+
+    time.sleep(2)
